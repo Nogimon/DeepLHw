57422dab70c336c004c660c25cc6637a395383da
Nogimon <x3722p@gmail.com>
2017-10-04 22:17:18 -0400

    first day

--- task1.py
+++ task1.py
@@ -5,16 +5,21 @@ import random
 from network import Network
 from mnist_loader import load_data, load_data_wrapper, vectorized_result
 
+#Task 1.2
 network = Network([784, 30, 10])
 training_data, validation_data, test_data = load_data_wrapper()
-print("task1.2:\nnow start to train\n", file = open("result.txt", "a"))
+print("\ntask1.2:\nnow start to train\n", file = open("result.txt", "a"))
 network.SGD(training_data, 30, 10, 3.0, test_data=test_data)
 
-print("task1.3\nnow start to train with 0 hidden layers\n", file = open("result.txt", "a"))
-network.SGD(training_data, 30, 0, 3.0, test_data = test_data)
+#Task 1.3
+print("\ntask1.3\nnow start to train with 0 hidden layers\n", file = open("result.txt", "a"))
+network1 = Network([784, 10])
+network1.SGD(training_data, 30, 10, 3.0, test_data = test_data)
 
-print("now start to train with learning rate = 30", file = open("result.txt", "a"))
-network.SGD(training_data, 30, 0, 30, test_data = test_data)
+print("\nnow start to train with learning rate = 30", file = open("result.txt", "a"))
+network.SGD(training_data, 30, 10, 30, test_data = test_data)
 
-print("now start to train with learning rate = 0.01", file = open("result.txt", "a"))
-network.SGD(training_data, 30, 0, 0.01, test_data = test_data)
+print("\nnow start to train with learning rate = 0.01", file = open("result.txt", "a"))
+network.SGD(training_data, 30, 10, 0.01, test_data = test_data)
+
+#Task 1.4
