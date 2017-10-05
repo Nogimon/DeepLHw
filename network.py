a347572f8a71b0ebbbcef30db0d4ca77e1b0be1d
Nogimon <x3722p@gmail.com>
2017-10-05 00:28:10 -0400

    first day

--- network.py
+++ network.py
@@ -132,6 +132,12 @@ def sigmoid(z):
     """The sigmoid function."""
     return 1.0/(1.0+np.exp(-z))
 
+#input an array, return a softmax array
+def softmax(z):
+    summation = np.sum(np.exp(z))
+    return (np.exp(z) / summation)
+
+
 def sigmoid_prime(z):
     """Derivative of the sigmoid function."""
     return sigmoid(z)*(1-sigmoid(z))
