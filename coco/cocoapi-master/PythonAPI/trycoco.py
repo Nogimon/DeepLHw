e4dbe2c5f866ccbe23fcd0945797e85700816ca5
Nogimon <x3722p@gmail.com>
2017-11-05 17:52:09 -0500

    start coco'

---
+++ coco/cocoapi-master/PythonAPI/trycoco.py
@@ -0,0 +1,41 @@
+from pycocotools.coco import COCO
+import numpy as np
+import skimage.io as io
+import matplotlib.pyplot as plt
+import pylab
+
+
+dire = '/media/zlab-1/Data/Lian/course/DeepLHw/coco/'
+dataDir=dire + 'images'
+dataType='val2014'
+annFile='{}/annotations/instances_{}.json'.format(dire,dataType)
+
+coco=COCO(annFile)
+
+cats = coco.loadCats(coco.getCatIds())
+nms=[cat['name'] for cat in cats]
+print('COCO categories: \n{}\n'.format(' '.join(nms)))
+
+nms = set([cat['supercategory'] for cat in cats])
+print('COCO supercategories: \n{}'.format(' '.join(nms)))
+
+catIds = coco.getCatIds(catNms=['person','dog','skateboard']);
+imgIds = coco.getImgIds(catIds=catIds );
+imgIds = coco.getImgIds(imgIds = [324158])
+img = coco.loadImgs(imgIds[np.random.randint(0,len(imgIds))])[0]
+
+
+# load and display image
+# I = io.imread('%s/images/%s/%s'%(dataDir,dataType,img['file_name']))
+# use url to load image
+I = io.imread(img['coco_url'])
+plt.axis('off')
+plt.imshow(I)
+plt.show()
+
+
+# load and display instance annotations
+plt.imshow(I); plt.axis('off')
+annIds = coco.getAnnIds(imgIds=img['id'], catIds=catIds, iscrowd=None)
+anns = coco.loadAnns(annIds)
+coco.showAnns(anns)
--- task1.py
+++ task1.py
@@ -6,75 +6,78 @@ from keras.layers import Dense, Activation, Conv2D, MaxPooling2D, Dropout, Flatt
 from keras.optimizers import Adam
 from keras.callbacks import ModelCheckpoint, EarlyStopping
 
+if __name__ == '__main__':
 
-(x_train, y_train), (x_test, y_test) = cifar10.load_data()
-y_train = keras.utils.to_categorical(y_train, num_classes = 10)
-y_test = keras.utils.to_categorical(y_test, num_classes = 10)
+	(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+	y_train = keras.utils.to_categorical(y_train, num_classes = 10)
+	y_test = keras.utils.to_categorical(y_test, num_classes = 10)
 
 
-model = Sequential()
+	model = Sequential()
 
-model.add(Conv2D(32, (3, 3), padding = 'same', input_shape = (32, 32, 3)))#input_shape = (28,28)))
-model.add(Activation('relu'))
-model.add(Conv2D(32, (3, 3), padding = 'same'))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size = (2, 2)))
-model.add(Dropout(0.25))
+	model.add(Conv2D(32, (3, 3), padding = 'same', input_shape = (32, 32, 3)))#input_shape = (28,28)))
+	model.add(Activation('relu'))
+	model.add(Conv2D(32, (3, 3), padding = 'same'))
+	model.add(Activation('relu'))
+	model.add(MaxPooling2D(pool_size = (2, 2)))
+	model.add(Dropout(0.25))
 
-model.add(ZeroPadding2D((1, 1)))
-model.add(Conv2D(64, (3, 3), padding = 'same'))
-model.add(Activation('relu'))
-model.add(Conv2D(64, (3, 3)))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size = (2, 2)))
-model.add(Dropout(0.25))
+	model.add(ZeroPadding2D((1, 1)))
+	model.add(Conv2D(64, (3, 3), padding = 'same'))
+	model.add(Activation('relu'))
+	model.add(Conv2D(64, (3, 3)))
+	model.add(Activation('relu'))
+	model.add(MaxPooling2D(pool_size = (2, 2)))
+	model.add(Dropout(0.25))
 
-model.add(ZeroPadding2D((1, 1)))
-model.add(Conv2D(128, (3, 3), padding = 'same'))
-model.add(Activation('relu'))
-model.add(Conv2D(128, (3, 3)))
-model.add(Activation('relu'))
-model.add(Conv2D(128, (3, 3)))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size = (2, 2)))
-model.add(Dropout(0.25))
-'''
-model.add(ZeroPadding2D((1, 1)))
-model.add(Conv2D(256, (3, 3), padding = 'same'))
-model.add(Activation('relu'))
-model.add(Conv2D(256, (3, 3)))
-model.add(Activation('relu'))
-model.add(Conv2D(256, (3, 3)))
-model.add(Activation('relu'))
-model.add(MaxPooling2D(pool_size = (2, 2)))
+	model.add(ZeroPadding2D((1, 1)))
+	model.add(Conv2D(128, (3, 3), padding = 'same'))
+	model.add(Activation('relu'))
+	model.add(Conv2D(128, (3, 3)))
+	model.add(Activation('relu'))
+	model.add(Conv2D(128, (3, 3)))
+	model.add(Activation('relu'))
+	model.add(MaxPooling2D(pool_size = (2, 2)))
+	model.add(Dropout(0.25))
+	'''
+	model.add(ZeroPadding2D((1, 1)))
+	model.add(Conv2D(256, (3, 3), padding = 'same'))
+	model.add(Activation('relu'))
+	model.add(Conv2D(256, (3, 3)))
+	model.add(Activation('relu'))
+	model.add(Conv2D(256, (3, 3)))
+	model.add(Activation('relu'))
+	model.add(MaxPooling2D(pool_size = (2, 2)))
 
-model.add(Flatten())
-model.add(Dense(2048))
-model.add(Dropout(0.5))
-'''
-model.add(Flatten())
-model.add(Dense(512))
-model.add(Dropout(0.5))
-model.add(Dense(10))
-model.add(Activation('softmax'))
+	model.add(Flatten())
+	model.add(Dense(2048))
+	model.add(Dropout(0.5))
+	'''
+	model.add(Flatten())
+	model.add(Dense(512))
+	model.add(Dropout(0.5))
+	model.add(Dense(10))
+	model.add(Activation('softmax'))
 
-model.compile(loss = 'categorical_crossentropy', optimizer = Adam(lr = 1e-5), metrics = ['accuracy'])
+	model.compile(loss = 'categorical_crossentropy', optimizer = Adam(lr = 1e-5), metrics = ['accuracy'])
 
-model_checkpoint = ModelCheckpoint('./weights_task4_1.h5'.format(), monitor='val_loss', save_best_only=True)
+	model_checkpoint = ModelCheckpoint('./weights_task4_1.h5'.format(), monitor='val_loss', save_best_only=True)
 
-model.fit(x_train, y_train, epochs = 50, batch_size = 10, callbacks=[model_checkpoint], validation_data=(x_test, y_test))
+	earlystop = EarlyStopping(patience=5, mode='auto')
 
-pre = model.predict(x_test)
-pre = [np.argmax(x) for x in pre]
-y = [np.argmax(y) for y in y_test]
+	model.fit(x_train, y_train, epochs = 150, batch_size = 10, callbacks=[model_checkpoint], validation_data=(x_test, y_test))
 
-acc = 0
-for i in range(len(pre)):
-    if pre[i] == y[i]:
-        acc+=1
+	pre = model.predict(x_test)
+	pre = [np.argmax(x) for x in pre]
+	y = [np.argmax(y) for y in y_test]
 
-score = model.evaluate(x_test, y_test, batch_size = 100)
+	acc = 0
+	for i in range(len(pre)):
+	    if pre[i] == y[i]:
+	        acc+=1
 
-print(acc)
+	score = model.evaluate(x_test, y_test, batch_size = 100)
+
+	print(acc)
 
 
